051e7c0b862d8cfeef412e8dd1b5e893a32ceab8
John Pawlak <johnpawlak@gmail.com>
2018-11-28 07:56:01 -0500

    setup formatting for amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,7 +1,9 @@
 //export a stateless functional component
 //description, amount, create
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({ description, amount, createdAt, id}) => {
     return (
@@ -10,7 +12,9 @@ const ExpenseListItem = ({ description, amount, createdAt, id}) => {
                 <h3>{description}</h3>
             </Link>
             
-            <p>{amount} - {createdAt}</p>
+            <p>{numeral(amount/100).format('$0,0.00')} 
+            - 
+            {moment(createdAt).format('MMMM Do, YYYY')}</p>
            
         </div>
     )
