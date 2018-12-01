c8d8c10f3853ed456dad957e36c3cfc3eca5a10c
John Pawlak <johnpawlak@gmail.com>
2018-12-01 10:02:28 -0500

    added asynchronous testing for expenses.js

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -2,11 +2,11 @@ const promise = new Promise((resolve, reject) => {
     //if we call resolve it went well, if reject, something went wrong: here's the error
     //promise can only be resolved or rejected, whichever happens first is what happens and ignores all else. can only pass one argument to resolve or reject
     setTimeout(() => {
-        // resolve({
-        //     name: 'john',
-        //     age: 29
-        // });
-        reject('something went wrong')
+        resolve({
+            name: 'john',
+            age: 29
+        });
+        // reject('something went wrong')
     }, 1500)
 });
 
@@ -14,6 +14,9 @@ console.log('before')
 //promise will fire only if resolved
 promise.then((data) => {
     console.log('1', data);
+    return 'some data'
+}).then((str) => {
+    console.log('does this run?', str)
 }).catch((err) => {
     console.log(err)
 })
--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,4 +1,10 @@
-import  { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import  { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import expenses from '../fixtures/expenses';
+import database from '../../firebase/firebase';
+
+const createMockStore = configureMockStore([thunk])
 
 test('should setup remove expense action object', () => {
     const action = removeExpense('123abc')
@@ -18,31 +24,48 @@ test('should setup edit expense action object', () => {
     })
 })
 test('should setup add expense action object with provided values', () => {
-    const expenseData = {
-        description: 'Rent',
-        amount: 109501,
-        createdAt: 1000,
-        note: 'this was last months rent'
-    }
-    const action = addExpense(expenseData)
+    const action = addExpense(expenses[2])
     expect(action).toEqual({
         type: 'ADD_EXPENSE',
-        expense: {
-            ...expenseData,
-            id: expect.any(String)
-        }
+        expense: expenses[2]
+    })
+});
+
+test('should add expense to database and store', (done) => {
+    const store = createMockStore({});
+    const expenseData = { description: "Mouse", amount: 3000, note: 'this one is better', createdAt: 1000 };
+
+    store.dispatch(startAddExpense(expenseData)).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type:'ADD_EXPENSE',
+            expense: {
+                id: expect.any(String), 
+                ...expenseData
+            }
+        })
+        database.ref(`expenses/${actions[0].expense.id}`).once('value').then((snapshot) => {
+            expect(snapshot.val()).toEqual(expenseData);
+            done();
+        })
+        
     })
 })
-test('should setup add expense action object with DEFAULT values', () => {
-    const action = addExpense()
-    expect(action).toEqual({
-        type: 'ADD_EXPENSE',
-        expense: {
-            description: '',
-            note: '',
-            amount: 0,
-            createdAt: 0,
-            id: expect.any(String)
-        }
-    })
+
+test('should add expense with defaults to database and store', () => {
+    
 })
+
+// test('should setup add expense action object with DEFAULT values', () => {
+//     const action = addExpense()
+//     expect(action).toEqual({
+//         type: 'ADD_EXPENSE',
+//         expense: {
+//             description: '',
+//             note: '',
+//             amount: 0,
+//             createdAt: 0,
+//             id: expect.any(String)
+//         }
+//     })
+// })
