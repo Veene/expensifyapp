6b74eb622b8d92bf8c240988b97997d86d86480f
John Pawlak <johnpawlak@gmail.com>
2018-12-05 12:31:45 -0500

    added reducer and action for auth testing login and logout, updated store and logic

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider, connect } from 'react-redux'
-import AppRouter from './routers/AppRouter';
+import { Provider } from 'react-redux'
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
+// import { addExpense } from './actions/expenses';
+import { login, logout } from './actions/auth';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'
@@ -31,19 +31,34 @@ const jsx = (
     <Provider store={store}>
         <AppRouter />
     </Provider>
-)
+);
+let hasRendered = false;
+const renderApp = () => {
+    if(!hasRendered) {
+        ReactDOM.render(jsx, document.getElementById('app'));
+        hasRendered = true;
+    }
+};
 
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-store.dispatch(startSetExpenses()).then(() => {
-    ReactDOM.render(jsx, document.getElementById('app'))
-})
+
 
 firebase.auth().onAuthStateChanged((user) => {
     if(user) {
-        //just logged in
+        store.dispatch(login(user.uid));
+        store.dispatch(startSetExpenses()).then(() => {
+            renderApp();
+            if(history.location.pathname === '/') {
+                history.push('/dashboard');
+            }
+        })
+        //checks current status of user when page loads and display message
         console.log('logged in');
     } else {
+        store.dispatch(logout());
+        renderApp();
+        history.push('/')
         console.log('logged out');
     }
 });
