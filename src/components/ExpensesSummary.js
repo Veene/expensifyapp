6e935df954dccf6a9d6cdfef9c65121958494b02
John Pawlak <johnpawlak@gmail.com>
2018-12-10 08:18:13 -0500

    styling header and dashboard

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
@@ -9,8 +10,14 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
     const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
 
     return (
-        <div>
-           <h1>You are viewing {expenseCount} {expenseWord} expenses for a total of {formattedExpensesTotal}</h1>
+        <div className="page-header">
+            <div className="content_container">
+                <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expenseWord} expenses totalling <span>{formattedExpensesTotal}</span></h1>
+                <div className="page-header__actions">
+                    <Link className="button" to="/create">Add Expense</Link>
+                </div>
+            </div>
+           
         </div>
     )
 }
